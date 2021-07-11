0382f689e9f7ab0083a1c4244dbdad68fe0a28ac
fenn <s.fenn@f5.com>
2021-07-11 12:38:59 -0700

    Changed dark/light mode button styles

--- my-portfolio-site/src/components/Buttons/DarkLightModeButton.js
+++ my-portfolio-site/src/components/Buttons/DarkLightModeButton.js
@@ -1,32 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 import SunIcon from "../../images/sun.svg";
+import { Wrapper, Shadow, ButtonEdge, ButtonFront } from "./NavBarButton";
 
 function tempAlert() {
   alert("That feature doesn't exist yet, sorry!");
 }
 
-const Wrapper = styled.button`
-  background-color: #e3ce63;
-  border-radius: 5px;
+const DarkLightWrapper = styled(Wrapper)`
   margin: 0 20px 0 10px;
-  box-shadow: 10px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const DarkLightButtonEdge = styled(ButtonEdge)`
+  background: linear-gradient(
+    to left,
+    hsl(50deg, 70%, 24%) 0%,
+    hsl(50deg, 70%, 44%) 8%,
+    hsl(50deg, 70%, 44%) 92%,
+    hsl(50deg, 70%, 24%) 100%
+  );
+`;
+
+const DarkLightButtonFront = styled(ButtonFront)`
   padding: calc(14px + (42 - 14) * ((100vw - 250px) / (1600 - 250)));
+  font-size: calc(12px + (30 - 12) * ((100vw - 250px) / (1600 - 250)));
   font-weight: 400;
-  cursor: pointer;
+  background: hsl(50deg, 70%, 64%);
   background-image: url(${SunIcon});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  transition: opacity 0.3s;
-  opacity: 1;
-  :hover {
-    opacity: 0.75;
-  }
 `;
 
 function DarkLightModeButton({ className }) {
-  return <Wrapper className={className} onClick={tempAlert} />;
+  return (
+    <DarkLightWrapper type="button" onClick={tempAlert} className={className}>
+      <Shadow></Shadow>
+      <DarkLightButtonEdge></DarkLightButtonEdge>
+      <DarkLightButtonFront></DarkLightButtonFront>
+    </DarkLightWrapper>
+  );
 }
 
 export default DarkLightModeButton;
--- my-portfolio-site/src/components/Buttons/ResumeButton.js
+++ my-portfolio-site/src/components/Buttons/ResumeButton.js
@@ -1,46 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { Wrapper, Shadow, ButtonEdge, ButtonFront } from "./NavBarButton";
 
-const Wrapper = styled.button`
-  position: relative;
-  background: transparent;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-  user-select: none;
-  -webkit-tap-highlight-color: transparent;
-  transition: filter 600ms;
-
-  &:hover {
-    transition: filter 250ms;
-    filter: brightness(110%);
-  }
-`;
-
-const Shadow = styled.span`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: hsl(0deg 0% 0% / 0.25);
-  border-radius: 4px;
-  transform: translateY(6px);
-  filter: blur(4px);
-
-  ${Wrapper}:hover &, ${Wrapper}:active & {
-    transform: translateY(10px);
-    transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
-  }
-`;
-
-const ButtonEdge = styled.span`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 4px;
+const ResumeButtonEdge = styled(ButtonEdge)`
   background: linear-gradient(
     to left,
     hsl(13deg, 48%, 28%) 0%,
@@ -50,27 +12,11 @@ const ButtonEdge = styled.span`
   );
 `;
 
-const ButtonFront = styled.span`
+const ResumeButtonFront = styled(ButtonFront)`
   padding: calc(4px + (12 - 4) * ((100vw - 250px) / (1600 - 250)))
     calc(10px + (40 - 10) * ((100vw - 250px) / (1600 - 250)));
   font-size: calc(12px + (30 - 12) * ((100vw - 250px) / (1600 - 250)));
-  display: block;
-  border-radius: 4px;
   background: hsl(13deg, 48%, 75%);
-  color: white;
-  will-change: transform;
-  transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
-  transform: translateY(-6px);
-
-  ${Wrapper}:hover &, ${Wrapper}:active & {
-    transform: translateY(-8px);
-    transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
-  }
-
-  ${Wrapper}:focus & {
-    transform: translateY(-4px);
-    transition: transform 34ms;
-  }
 `;
 
 function goToResume() {
@@ -89,10 +35,10 @@ function ResumeButton({ className }) {
   return (
     <Wrapper type="button" onClick={goToResume} className={className}>
       <Shadow></Shadow>
-      <ButtonEdge></ButtonEdge>
-      <ButtonFront>
+      <ResumeButtonEdge></ResumeButtonEdge>
+      <ResumeButtonFront>
         <ButtonText>Resume</ButtonText>
-      </ButtonFront>
+      </ResumeButtonFront>
     </Wrapper>
   );
 }
